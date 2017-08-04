2ead15eaf712f60a6e9a25009ca60c2e5d7e1621
heinousdog <heinousdog@gmail.com>
2017-08-04 17:07:08 +0800

    New test

--- MyProject/BookBestSeller.js
+++ MyProject/BookBestSeller.js
@@ -4,6 +4,7 @@ import Bookitem from './Bookitem';
 const API_Key='73b19491b83909c7e07016f4bb4644f9:2:60667290';
 const Query_Type='hardcover-fiction';
 const API_stem='https://api.nytimes.com/svc/books/v3/lists';
+
 //const Path=`${API_stem}/${Query_Type}?response-format=json&api-key=${API_Key}`;
 
 // 這是紐約時報推薦書籍 
@@ -11,12 +12,12 @@ const API_stem='https://api.nytimes.com/svc/books/v3/lists';
 // 
 
 //這是雅虎天氣
-const Path='https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+//const Path='https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
  //const Path='https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D2502265&format=json&diagnostics=true&callback='
 
 // google stock information
-// const Path='https://finance.google.com/finance/info?client=ig&q=TPE:2884'
-
+ const Path='https://query.yahooapis.com/v1/public/yql?format=json&env=store://datatables.org/alltableswithkeys&q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(\'2884.TW\')'
+ //const Path = 'http://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20170802&stockNo=2884'
 export default class BookBestSeller extends Component {
 constructor(props) {
     super(props);
@@ -29,12 +30,13 @@ componentDidMount() {
     //return fetch('https://facebook.github.io/react-native/movies.json')
     return fetch(Path)
       .then((response) => response.json())
+
       .then((responseJson) => {
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.setState({
           isLoading: false,
           //dataSource: ds.cloneWithRows(responseJson.movies),
-        dataSource: ds.cloneWithRows(responseJson.query.results.channel),
+        dataSource: ds.cloneWithRows(responseJson.query.results),
         //dataSource: ds.cloneWithRows(responseJson),
         }, function() {
           // do something with new state
@@ -80,10 +82,10 @@ render(){
 
         return(
             <View >
-              
+             
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.distance},{rowData.pressure},{rowData.speed},{rowData.country}</Text>}
+          renderRow={(rowData) => <Text>{rowData.symbol},{rowData.Name},{rowData.LastTradePriceOnly},{rowData.Change}</Text>}
            //renderRow={(rowData) => <Text>{rowData.title}, {rowData.releaseYear}</Text>}
            //renderRow={(rowData) => <Text>{rowData.lt},{rowData.l_cur},{rowData.t},{rowData.id}</Text>}
         />
